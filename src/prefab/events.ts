3d7ab7992218168ac1f4e8fc05ca7aa94cbffff7
Aaron Li-Mai <aaronmai@hotmail.com>
2022-01-06 17:45:39 -0700

    wording updates as well as make sure gambling is run in correct channel

--- src/prefab/events.ts
+++ src/prefab/events.ts
@@ -7,8 +7,8 @@ import {
   Message,
   TextChannel
 } from "discord.js";
+import { NO_GAMBLING_CHANNEL_SET } from "../../config/messages.json";
 import { Client } from "../util/client";
-import { KelleeBotCommand } from "../util/command";
 
 const getDefaultChannel = (guild: Guild): TextChannel => {
   let channel;
@@ -81,7 +81,14 @@ async function guildCreate(client: Client, guild: Guild) {
 
 async function interactionCreate(client: Client, interaction: Interaction) {
   try {
-    if (interaction.isAutocomplete()) {
+    const userInfo = await client.profileInfo.get(interaction.user.id)
+    const botOwner = client.users.cache.get(client.config.devs[0])
+
+    if (interaction.isSelectMenu()) {
+      //TODO
+    } else if (interaction.isButton()) {
+      //TODO
+    } else if (interaction.isAutocomplete()) {
       const command = client.commands.get(interaction.commandName);
       if (!command) return;
 
@@ -95,145 +102,163 @@ async function interactionCreate(client: Client, interaction: Interaction) {
       if (sub && sub.isAutocomplete && sub.autocomplete) {
         await sub.autocomplete({ client, interaction })
       }
-    }
+    } else if (interaction.isCommand()) {
+      if (userInfo.isBlacklisted) return;
 
-    const userInfo = await client.profileInfo.get(interaction.user.id)
-    if (userInfo.isBlacklisted) return;
-
-    if (interaction.isCommand()) {
       const command = client.commands.get(interaction.commandName);
-
       if (!command) return;
 
-      if (!client.config.devs.includes(interaction.user.id)) {
-        if (command.guildOnly) {
-          if (!interaction.inGuild()) return;
-
-          if (
-            command.ownerOnly &&
-            interaction.guild!.ownerId !== interaction.user.id
-          )
-            return await quickReply(
-              client,
-              interaction,
-              "This command can only be used by the server owner."
-            );
-
-          const guildInfo = await client.guildInfo.get(interaction.guildId);
-
-          if (guildInfo.disabledCommands.includes(command.name))
-            return await quickReply(
-              client,
-              interaction,
-              "This command is currently disabled in this server."
-            );
-          if (
-            guildInfo.disabledChannels.includes(
-              interaction.channelId
-            ) &&
-            !command.ignoreDisabledChannels
-          )
-            return await quickReply(
-              client,
-              interaction,
-              "You can't use any commands in this channel."
-            );
-
-          //@ts-ignore
-          if (
-            command.clientPerms &&
-            !interaction.channel!
-              .permissionsFor(interaction.guild!.me!)
-              .has(command.clientPerms, true)
-          )
-            return await quickReply(
-              client,
-              interaction,
-              `I am missing the following permissions: ${client.utils.missingPermissions(
-                interaction.guild!.me!,
-                command.clientPerms
-              )}.`
-            );
-
-          if (
-            guildInfo.commandPerms &&
-            guildInfo.commandPerms[command.name] &&
-            //@ts-ignore
-            !interaction.member.permissions.has(
-              guildInfo.commandPerms[command.name],
-              true
-            )
-          )
-            return await quickReply(
-              client,
-              interaction,
-              `You are missing the following permissions: ${client.utils.missingPermissions(
-                interaction.member! as GuildMember,
-                guildInfo.commandPerms[command.name]
-              )}.`
-            );
-          else if (
-            command.perms &&
-            //@ts-ignore
-            !interaction.member.permissions.has(command.perms, true)
-          )
-            return await quickReply(
-              client,
-              interaction,
-              `You are missing the following permissions: ${client.utils.missingPermissions(
-                interaction.member as GuildMember,
-                command.perms
-              )}.`
-            );
-
-          //@ts-ignore
-          if (command.nsfw && !interaction.channel.nsfw)
-            return await quickReply(
-              client,
-              interaction,
-              `This command may only be used in an NSFW channel.`
-            );
+      // if (!client.config.devs.includes(interaction.user.id)) {
+      if (command.guildOnly) {
+        if (!interaction.inGuild()) {
+          return interaction.reply({
+            content: "Slash commands can only be used within servers.",
+            ephemeral: true
+          });
         }
 
-        const cd = await client.utils.getCooldown(command, interaction);
+        const guildInfo = await client.guildInfo.get(interaction.guildId);
 
-        let cooldowns;
-        if (cd) {
-          if (
-            typeof command.globalCooldown === "undefined" ||
-            command.globalCooldown
-          ) {
-            if (!client.globalCooldowns.has(command.name))
-              client.globalCooldowns.set(command.name, new Collection());
-            cooldowns = client.globalCooldowns;
+        if (command.devOnly && !client.config.devs.includes(interaction.user.id)) {
+          return await quickReply(client, interaction, `Nice try, but only ${botOwner?.tag} can use this command.`);
+        }
+
+        if (
+          command.ownerOnly &&
+          interaction.guild!.ownerId !== interaction.user.id
+        )
+          return await quickReply(
+            client,
+            interaction,
+            "This command can only be used by the server owner."
+          );
+
+        if (guildInfo.disabledCommands.includes(command.name))
+          return await quickReply(
+            client,
+            interaction,
+            "This command is currently disabled in this server."
+          );
+
+        if (
+          guildInfo.disabledChannels.includes(
+            interaction.channelId
+          ) &&
+          !command.ignoreDisabledChannels
+        )
+          return await quickReply(
+            client,
+            interaction,
+            "You can't use any commands in this channel."
+          );
+
+        //@ts-ignore
+        if (
+          command.clientPerms &&
+          !interaction.channel!
+            .permissionsFor(interaction.guild!.me!)
+            .has(command.clientPerms, true)
+        )
+          return await quickReply(
+            client,
+            interaction,
+            `I can't execute this command as I am missing the following permissions: ${client.utils.missingPermissions(
+              interaction.guild!.me!,
+              command.clientPerms
+            )}.`
+          );
+
+        if (
+          guildInfo.commandPerms &&
+          guildInfo.commandPerms[command.name] &&
+          //@ts-ignore
+          !interaction.member.permissions.has(
+            guildInfo.commandPerms[command.name],
+            true
+          )
+        )
+          return await quickReply(
+            client,
+            interaction,
+            `Woah there! Nice try, but you don't have the proper permissions to execute this command. You'll need one of the following permissions: ${client.utils.missingPermissions(
+              interaction.member! as GuildMember,
+              guildInfo.commandPerms[command.name]
+            )}.`
+          );
+        else if (
+          command.perms &&
+          //@ts-ignore
+          !interaction.member.permissions.has(command.perms, true)
+        )
+          return await quickReply(
+            client,
+            interaction,
+            `Woah there! Nice try, but you don't have the proper permissions to execute this command. You'll need one of the following permissions: ${client.utils.missingPermissions(
+              interaction.member as GuildMember,
+              command.perms
+            )}.`
+          );
+
+        //@ts-ignore
+        if (command.nsfw && !interaction.channel.nsfw)
+          return await quickReply(
+            client,
+            interaction,
+            `This command may only be used in an NSFW channel.`
+          );
+
+        if (command.category === "Gambling") {
+          const { gamblingChannel } = guildInfo.gambling
+          if (gamblingChannel) {
+            if (interaction.channelId !== gamblingChannel) {
+              return await quickReply(client, interaction, `This command can only be used in <#${gamblingChannel}>!`)
+            }
           } else {
-            if (!client.serverCooldowns.has(interaction.guildId!))
-              client.serverCooldowns.set(
-                interaction.guildId!,
-                new Collection()
-              );
-            cooldowns = client.serverCooldowns.get(interaction.guildId!);
-            if (!cooldowns!.has(command.name))
-              cooldowns!.set(command.name, new Collection());
-          }
-
-          const now = Date.now();
-          const timestamps = cooldowns!.get(command.name);
-          const cooldownAmount = cd * 1000;
-          if (timestamps!.has(interaction.user.id)) {
-            const expirationTime =
-              timestamps!.get(interaction.user.id)! + cooldownAmount;
-            if (now < expirationTime)
-              return await quickReply(
-                client,
-                interaction,
-                `Please wait \`${client.utils.msToTime(
-                  expirationTime - now
-                )}\` before using this command again.`
-              );
+            return await quickReply(client, interaction, NO_GAMBLING_CHANNEL_SET);
           }
         }
       }
+      // }
+
+      const cd = await client.utils.getCooldown(command, interaction);
+
+      let cooldowns;
+      if (cd) {
+        if (
+          typeof command.globalCooldown === "undefined" ||
+          command.globalCooldown
+        ) {
+          if (!client.globalCooldowns.has(command.name))
+            client.globalCooldowns.set(command.name, new Collection());
+          cooldowns = client.globalCooldowns;
+        } else {
+          if (!client.serverCooldowns.has(interaction.guildId!))
+            client.serverCooldowns.set(
+              interaction.guildId!,
+              new Collection()
+            );
+          cooldowns = client.serverCooldowns.get(interaction.guildId!);
+          if (!cooldowns!.has(command.name))
+            cooldowns!.set(command.name, new Collection());
+        }
+
+        const now = Date.now();
+        const timestamps = cooldowns!.get(command.name);
+        const cooldownAmount = cd * 1000;
+        if (timestamps!.has(interaction.user.id)) {
+          const expirationTime =
+            timestamps!.get(interaction.user.id)! + cooldownAmount;
+          if (now < expirationTime)
+            return await quickReply(
+              client,
+              interaction,
+              `Please wait \`${client.utils.msToTime(
+                expirationTime - now
+              )}\` before using this command again.`
+            );
+        }
+      }
+      // }
 
       const group = interaction.options.getSubcommandGroup(false)!;
       const subcommand = interaction.options.getSubcommand(false)!;
