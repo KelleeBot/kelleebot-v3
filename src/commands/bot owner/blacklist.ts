57e7790627ea3f96d279f9609d31a343906f4bd5
Aaron Li-Mai <aaronmai@hotmail.com>
2022-01-06 18:40:22 -0700

    added blacklist commands

---
+++ src/commands/bot owner/blacklist.ts
@@ -0,0 +1,46 @@
+import { CommandInteraction } from "discord.js";
+import { Client } from "../../util/client";
+import { KelleeBotCommand } from "../../util/command";
+import { add, remove } from ".";
+
+export default class Blacklist extends KelleeBotCommand {
+    constructor(client: Client) {
+        super(client, {
+            name: "blacklist",
+            category: "Bot Owner",
+            description: "Blacklists/whitelists users from using the bot.",
+            devOnly: true,
+            clientPerms: ["SEND_MESSAGES"],
+            subcommands: {
+                add: {
+                    description: "Blacklist a user.",
+                    args: [
+                        {
+                            name: "user",
+                            description: "The user to add to the blacklist.",
+                            type: "USER",
+                            required: true
+                        }
+                    ],
+                    execute: async ({ client, interaction }) => {
+                        await add(client, interaction);
+                    }
+                },
+                remove: {
+                    description: "Whitelist a user.",
+                    args: [
+                        {
+                            name: "user",
+                            description: "The user to remove from the blacklist.",
+                            type: "USER",
+                            required: true
+                        }
+                    ],
+                    execute: async ({ client, interaction }) => {
+                        await remove(client, interaction);
+                    }
+                }
+            }
+        });
+    }
+}
---
+++ src/commands/bot owner/remove.ts
@@ -0,0 +1,18 @@
+import { Client } from "../../util/client"
+import { CommandInteraction } from "discord.js"
+
+export const remove = async (client: Client, interaction: CommandInteraction) => {
+    const user = interaction.options.getUser("user")!;
+    const userInfo = await client.profileInfo.get(user.id);
+
+    if (!userInfo.isBlacklisted) return interaction.reply({ content: `**${user.tag}** is currently not blacklisted.` })
+
+    userInfo.isBlacklisted = false
+    await client.profileInfo.findByIdAndUpdate(
+        user.id,
+        { $set: { isBlacklisted: false } },
+        { new: true, upsert: true, setDefaultsOnInsert: true }
+    );
+
+    return interaction.reply({ content: `You have successfully whitelisted **${user.tag}**.` });
+}
