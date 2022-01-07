b72992db71927f904dd2548061c214425b09853b
Aaron Li-Mai <aaronmai@hotmail.com>
2022-01-06 18:22:52 -0700

    added additional helper methods

--- src/util/helpers/gambling.ts
+++ src/util/helpers/gambling.ts
@@ -12,7 +12,41 @@ export const addPoints = async (guildID: Snowflake, userID: Snowflake, points: n
             $inc: { points }
         },
         { new: true, upsert: true }
-    )
+    );
     pointsCache[`${guildID}-${userID}`] = result.points
-    return result.points
+    return result.points;
 }
+
+export const setPoints = async (guildID: Snowflake, userID: Snowflake, points: number) => {
+    const result = await gambling.findOneAndUpdate(
+        { guildID, userID },
+        {
+            guildID,
+            userID,
+            $set: { points }
+        },
+        { new: true, upsert: true }
+    );
+    pointsCache[`${guildID}-${userID}`] = result.points;
+    return result.points;
+}
+
+export const getPoints = async (guildID: Snowflake, userID: Snowflake) => {
+    const cachedValue = pointsCache[`${guildID}-${userID}`];
+    if (cachedValue) return cachedValue;
+
+    const result = await gambling.findOne({ guildID, userID });
+
+    let points = 0;
+    if (result) {
+        points = result.points;
+    } else {
+        await new gambling({
+            guildID,
+            userID,
+            points
+        }).save();
+    }
+    pointsCache[`${guildID}-${userID}`] = points;
+    return points;
+}; 
