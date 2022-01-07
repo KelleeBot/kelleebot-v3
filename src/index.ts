94aad89e6d98e8a1e1eecc9512dddfba5aba392c
alima566 <aaronmai@hotmail.com>
2022-01-07 15:03:19 -0700

    updated client options

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,20 @@ import "./util/prototype";
 import dotenv from "dotenv";
 dotenv.config();
 
-const client = new Client({ intents: Object.values(Intents.FLAGS) });
+const client = new Client({
+  intents: [
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MEMBERS,
+    Intents.FLAGS.GUILD_BANS,
+    Intents.FLAGS.GUILD_PRESENCES,
+    Intents.FLAGS.GUILD_MESSAGES,
+    Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+    Intents.FLAGS.GUILD_VOICE_STATES,
+    Intents.FLAGS.GUILD_WEBHOOKS
+  ],
+  partials: ["MESSAGE", "CHANNEL", "REACTION", "USER", "GUILD_MEMBER"],
+  allowedMentions: { parse: ["users", "roles"], repliedUser: false }
+});
 
 (async () => {
   client.login(process.env.DISCORD_TOKEN);
