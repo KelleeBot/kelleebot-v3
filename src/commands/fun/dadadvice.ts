29c4d56b688c5ff1c601c3e5327efad953e81ba4
Aaron Li-Mai <aaronmai@hotmail.com>
2022-01-08 21:00:30 -0700

    added dadjoke and dadadvice commands

---
+++ src/commands/fun/dadadvice.ts
@@ -0,0 +1,28 @@
+import axios from "axios";
+import { CommandInteraction } from "discord.js";
+import { Client } from "../../util/client";
+import { KelleeBotCommand } from "../../util/command";
+
+export default class Dadadvice extends KelleeBotCommand {
+    constructor(client: Client) {
+        super(client, {
+            name: "dadadvice",
+            category: "fun",
+            cooldown: 15,
+            clientPerms: ["SEND_MESSAGES"],
+            description: `${client.user!.username} tells you some advice.`
+        });
+    }
+    async execute({ client, interaction }: { client: Client, interaction: CommandInteraction }) {
+        this.setCooldown(interaction);
+        await interaction.deferReply();
+        try {
+            const resp = await axios.get("https://api.adviceslip.com/advice");
+            const { data } = resp;
+            return interaction.editReply({ content: data.slip.advice });
+        } catch (e) {
+            client.utils.log("ERROR", `${__filename}`, `An error has occurred: ${e}`);
+            return interaction.editReply({ content: "An error has occurred. Please try again." });
+        }
+    }
+}
---
+++ src/commands/fun/dadjoke.ts
@@ -0,0 +1,30 @@
+import axios from "axios";
+import { CommandInteraction } from "discord.js";
+import { Client } from "../../util/client";
+import { KelleeBotCommand } from "../../util/command";
+
+export default class Dadjoke extends KelleeBotCommand {
+    constructor(client: Client) {
+        super(client, {
+            name: "dadjoke",
+            category: "fun",
+            cooldown: 15,
+            clientPerms: ["SEND_MESSAGES"],
+            description: `${client.user!.username} tells you a dad joke.`
+        });
+    }
+    async execute({ client, interaction }: { client: Client, interaction: CommandInteraction }) {
+        this.setCooldown(interaction);
+        await interaction.deferReply();
+        try {
+            const resp = await axios.get("https://icanhazdadjoke.com/", {
+                headers: { "Accept": "application/json", "User-Agent": `${client.user!.tag}` },
+            });
+            const { data } = resp;
+            return interaction.editReply({ content: data.joke });
+        } catch (e) {
+            client.utils.log("ERROR", `${__filename}`, `An error has occurred: ${e}`);
+            return interaction.editReply({ content: "An error has occurred. Please try again." });
+        }
+    }
+}
