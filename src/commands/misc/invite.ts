7e9af79fe8d7917b15a3ea3ac0b5b28edf90fb18
Aaron Li-Mai <aaronmai@hotmail.com>
2022-01-08 18:30:55 -0700

    added invite command

---
+++ src/commands/misc/invite.ts
@@ -0,0 +1,30 @@
+import { CommandInteraction, MessageActionRow, MessageButton, Permissions } from "discord.js";
+import { Client } from "../../util/client";
+import { KelleeBotCommand } from "../../util/command";
+
+export default class Invite extends KelleeBotCommand {
+    constructor(client: Client) {
+        super(client, {
+            name: "invite",
+            category: "Misc",
+            description: "Invite me into your server!"
+        });
+    }
+    async execute({ client, interaction }: { client: Client, interaction: CommandInteraction }) {
+        const inviteLink = client.generateInvite({
+            scopes: ["bot", "applications.commands"],
+            permissions: [Permissions.FLAGS.ADMINISTRATOR]
+        });
+
+        const msgEmbed = (await client.utils.CustomEmbed({ userID: interaction.user.id }))
+            .setTitle("Invite Me!")
+            .setThumbnail(client.user?.displayAvatarURL({ dynamic: true })!)
+            .setDescription("If you would like me in your server, all you have to do is click on the button below and I will automatically join your server!");
+
+        const button = new MessageActionRow().addComponents(
+            new MessageButton().setLabel("Invite").setStyle("LINK").setURL(inviteLink)
+        );
+
+        return await interaction.reply({ embeds: [msgEmbed], components: [button], ephemeral: true });
+    }
+}
