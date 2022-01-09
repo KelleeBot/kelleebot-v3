89a56c948d10476de132e6693f9b8bdf67a40027
Aaron Li-Mai <aaronmai@hotmail.com>
2022-01-08 21:37:27 -0700

    added restart command

---
+++ src/commands/bot owner/restart.ts
@@ -0,0 +1,18 @@
+import { CommandInteraction } from "discord.js";
+import { Client } from "../../util/client";
+import { KelleeBotCommand } from "../../util/command";
+
+export default class Restart extends KelleeBotCommand {
+    constructor(client: Client) {
+        super(client, {
+            name: "restart",
+            category: "Bot Owner",
+            devOnly: true,
+            hideCommand: true,
+            description: "You can't use this command, so why bother explaining."
+        });
+    }
+    async execute({ interaction }: { interaction: CommandInteraction }) {
+        interaction.reply({ content: "Restarting...", ephemeral: true }).then(() => process.exit());
+    }
+}
