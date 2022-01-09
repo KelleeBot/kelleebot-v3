43505a3c0bd2ddf559685a61726d9f6ca163838e
Aaron Li-Mai <aaronmai@hotmail.com>
2022-01-09 00:23:02 -0700

    added twitter initializer

--- src/util/client.ts
+++ src/util/client.ts
@@ -1,15 +1,17 @@
-import { ClientOptions, Collection, Snowflake } from "discord.js";
+import { ClientOptions } from "discord.js";
 import TwitchApi from "node-twitch";
 import { KelleeBotClient } from "../prefab/client";
 import { Manager } from "../prefab/manager";
 import TwitchLiveModel from "../schemas/twitchLive";
 import { TwitchLive } from "../types/twitchLive"
 import { MovieDb } from "moviedb-promise";
+import Twitter from "twitter-lite";
 
 class Client extends KelleeBotClient {
   twitchLiveInfo: Manager<string, TwitchLive>;
   twitchApi: TwitchApi;
   movieDb: MovieDb;
+  twitter: Twitter;
 
   constructor(options: ClientOptions) {
     super(options);
@@ -21,6 +23,12 @@ class Client extends KelleeBotClient {
       // access_token: `${process.env.TWITCH_BEARER_TOKEN}`
     });
     this.movieDb = new MovieDb(`${process.env.MOVIEDB_API_KEY}`);
+    this.twitter = new Twitter({
+      consumer_key: `${process.env.TWITTER_API_KEY}`,
+      consumer_secret: `${process.env.TWITTER_API_KEY_SECRET}`,
+      access_token_key: `${process.env.TWITTER_ACCESS_TOKEN}`,
+      access_token_secret: `${process.env.TWITTER_ACCESS_TOKEN_SECRET}`
+    });
   }
 }
 
