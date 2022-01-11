9054638c79061289a41917317a0ec7ee65f6ae08
alima566 <aaronmai@hotmail.com>
2022-01-11 14:31:17 -0700

    updated messageCreate event

--- src/events/message/messageCreate.ts
+++ src/events/message/messageCreate.ts
@@ -1,7 +1,28 @@
 import { Message } from "discord.js";
-import { messageCreate } from "../../prefab/events";
+//import { messageCreate } from "../../prefab/events";
 import { Client } from "../../util/client";
+import axios from "axios";
 
 export default async (client: Client, message: Message) => {
-  await messageCreate(client, message);
+  //await messageCreate(client, message);
+  try {
+    const userInfo = await client.profileInfo.get(message.author.id);
+
+    if (message.author.bot || message.channel.type === "DM" || userInfo.isBlacklisted) return;
+
+    const guildInfo = await client.guildInfo.get(message.guildId!);
+    if (!guildInfo.botChatChannel) return;
+
+    if (guildInfo.botChatChannel === message.channelId) {
+      message.channel.sendTyping();
+      const resp = await axios.get(`https://api.deltaa.me/chatbot?message=${encodeURIComponent(
+        message.content
+      )}&name=${encodeURIComponent(client.user!.username)}&user=${message.author.id
+        }&gender=Female`);
+
+      return message.reply({ content: resp.data.message });
+    }
+  } catch (e) {
+    return client.utils.log("ERROR", `${__filename}`, `An error has occurred: ${e}`);
+  }
 };
