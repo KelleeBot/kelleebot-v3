af13f4467021c8a6b39da285106c311ef65036f0
alima566 <aaronmai@hotmail.com>
2022-01-11 10:44:30 -0700

    added add and set commands for gambling

---
+++ src/commands/gambling/add.ts
@@ -0,0 +1,61 @@
+import { CommandInteraction, GuildMember } from "discord.js";
+import { Client } from "../../util/client";
+import { KelleeBotCommand } from "../../util/command";
+import { addPoints } from "../../util";
+import { GREATER_THAN_ZERO } from "../../../config/messages.json"
+
+export default class Add extends KelleeBotCommand {
+    constructor(client: Client) {
+        super(client, {
+            name: "add",
+            category: "Gambling",
+            description: "Add points to members.",
+            perms: ["MANAGE_GUILD"],
+            clientPerms: ["SEND_MESSAGES"],
+            options: [
+                {
+                    name: "points",
+                    description: "The amount of points to give.",
+                    type: "NUMBER",
+                    required: true
+                },
+                {
+                    name: "user",
+                    description: "The user to give points to. Defaults 'all' if no user specified.",
+                    type: "USER"
+                }
+            ]
+        });
+    }
+    async execute({ client, interaction }: { client: Client, interaction: CommandInteraction }) {
+        const points = interaction.options.getNumber("points")!;
+        const member = interaction.options.getMember("user") ?? "all";
+
+        if (points <= 0)
+            return interaction.reply({ content: GREATER_THAN_ZERO });
+
+        try {
+            if (member === "all") {
+                const members = await interaction.guild?.members.fetch();
+                members?.forEach(async (member) => {
+                    if (!member.user.bot)
+                        await addPoints(interaction.guildId!, member.id, points);
+                });
+
+                const memberCount = members?.filter(member => !member.user.bot).size!;
+                return interaction.reply({ content: `You have successfully added ${client.utils.pluralize(points, "point", true)} to ${client.utils.pluralize(memberCount, "member")}.` });
+            }
+
+            if ((member as GuildMember).user.bot)
+                return interaction.reply({ content: "You cannot give points to bots." });
+
+            const newPoints = await addPoints(interaction.guildId!, (member as GuildMember).id, points);
+            return interaction.reply({
+                content: `You have given **${(member as GuildMember).user.tag}** ${client.utils.pluralize(points, "point", true)}. They now have ${client.utils.pluralize(newPoints, "point")}.`
+            });
+        } catch (e) {
+            client.utils.log("ERROR", `${__filename}`, `An error has occurred: ${e}`);
+            return interaction.reply({ content: "An error has occurred. Please try again." });
+        }
+    }
+};
---
+++ src/commands/gambling/set.ts
@@ -0,0 +1,61 @@
+import { CommandInteraction, GuildMember } from "discord.js";
+import { Client } from "../../util/client";
+import { KelleeBotCommand } from "../../util/command";
+import { setPoints } from "../../util";
+import { GREATER_THAN_ZERO } from "../../../config/messages.json"
+
+export default class Set extends KelleeBotCommand {
+    constructor(client: Client) {
+        super(client, {
+            name: "set",
+            category: "Gambling",
+            description: "Set's points to members.",
+            perms: ["MANAGE_GUILD"],
+            clientPerms: ["SEND_MESSAGES"],
+            options: [
+                {
+                    name: "points",
+                    description: "The amount of points to set.",
+                    type: "NUMBER",
+                    required: true
+                },
+                {
+                    name: "user",
+                    description: "The user to set points to. Defaults 'all' if no user specified.",
+                    type: "USER"
+                }
+            ]
+        });
+    }
+    async execute({ client, interaction }: { client: Client, interaction: CommandInteraction }) {
+        const points = interaction.options.getNumber("points")!;
+        const member = interaction.options.getMember("user") ?? "all";
+
+        if (points < 0)
+            return interaction.reply({ content: GREATER_THAN_ZERO });
+
+        try {
+            if (member === "all") {
+                const members = await interaction.guild?.members.fetch();
+                members?.forEach(async (member) => {
+                    if (!member.user.bot)
+                        await setPoints(interaction.guildId!, member.id, points);
+                });
+
+                const memberCount = members?.filter(member => !member.user.bot).size!;
+                return interaction.reply({ content: `You have successfully set ${client.utils.pluralize(memberCount, "member")} points to \`${client.utils.formatNumber(points)}\`.` });
+            }
+
+            if ((member as GuildMember).user.bot)
+                return interaction.reply({ content: "You cannot set bot's points." });
+
+            const newPoints = await setPoints(interaction.guildId!, (member as GuildMember).id, points);
+            return interaction.reply({
+                content: `Points have been successfully set to \`${client.utils.formatNumber(newPoints)}\` for **${(member as GuildMember).user.tag}**.`
+            });
+        } catch (e) {
+            client.utils.log("ERROR", `${__filename}`, `An error has occurred: ${e}`);
+            return interaction.reply({ content: "An error has occurred. Please try again." });
+        }
+    }
+}
