8fb94213d448db339742d40ce0967d83310b9765
alima566 <aaronmai@hotmail.com>
2022-01-12 15:48:21 -0700

    renamed file

---
+++ src/types/channelTypes.d.ts
@@ -0,0 +1,14 @@
+export type ChannelTypes = (
+    | "DM"
+    | "GROUP_DM"
+    | "GUILD_TEXT"
+    | "GUILD_VOICE"
+    | "GUILD_CATEGORY"
+    | "GUILD_NEWS"
+    | "GUILD_STORE"
+    | "GUILD_NEWS_THREAD"
+    | "GUILD_PUBLIC_THREAD"
+    | "GUILD_PRIVATE_THREAD"
+    | "GUILD_STAGE_VOICE"
+    | "UNKNOWN"
+)
