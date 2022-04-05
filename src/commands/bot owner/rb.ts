c9dabb6c098f99650f3de910892f46f0ad2bc48a
alima566 <aaronmai@hotmail.com>
2022-04-05 09:54:04 -0600

    Specify which guild and channel to send message to

--- src/commands/bot owner/rb.ts
+++ src/commands/bot owner/rb.ts
@@ -13,15 +13,25 @@ export default class Rb extends KelleeBotCommand {
             description: "You can't use this command, so why bother explaining.",
             options: [
                 {
-                    name: "channel",
+                    name: "guildid",
+                    description: "The guild to send the message to.",
+                    type: "STRING",
+                    required: true,
+                },
+                {
+                    name: "channelid",
                     description: "The channel to send the message to.",
-                    type: "CHANNEL",
-                    channelTypes: ["GUILD_TEXT"]
+                    type: "STRING",
+                    // type: "CHANNEL",
+                    // channelTypes: ["GUILD_TEXT"],
+                    required: true
                 }
             ],
             execute: async ({ interaction }) => {
                 try {
-                    const channel = interaction.options.getChannel("channel") ?? interaction.channel;
+                    const guildID = interaction.options.getString("guildid")!;
+                    const channelID = interaction.options.getString("channelid")!;
+                    //const channel = interaction.options.getChannel("channel")!; // ?? interaction.channel;
 
                     const button = new MessageActionRow().addComponents(
                         new MessageButton()
@@ -30,8 +40,16 @@ export default class Rb extends KelleeBotCommand {
                             .setStyle("PRIMARY")
                     );
 
+                    const guild = await client.guilds.fetch(guildID);
+                    if (!guild) return await interaction.reply({ content: "A guild with that ID was not found.", ephemeral: true });
+
+                    const channel = await guild.channels.fetch(channelID);
+                    if (!channel) return await interaction.reply({ content: "A channel with that ID was not found.", ephemeral: true });
+
+                    if (channel.type !== "GUILD_TEXT") return await interaction.reply({ content: "Please ensure that the channel is a text channel.", ephemeral: true });
+
                     await interaction.reply({ content: `Role button message successully sent to ${channel}.`, ephemeral: true });
-                    return await (channel as TextChannel).send({ content: "Unsure which roles you currently already have? Click here:", components: [button] });
+                    return await channel.send({ content: "Unsure which roles you currently already have? Click here:", components: [button] });
                 } catch (e) {
                     client.utils.log("ERROR", `${__filename}`, `An error has occurred: ${e}`);
                     return await interaction.reply({ content: "An error has occurred. Please try again.", ephemeral: true });
